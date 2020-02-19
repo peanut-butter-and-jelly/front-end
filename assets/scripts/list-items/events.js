23db6e42e0c279c88ffab949f71add53a58b6632
Francis A <frazem60@gmail.com>
2020-02-19 10:43:28 -0500

    Finish CRUD on list
    -Update list item
    -Created modal for update list item
    -Automatically refresh list on update,create, delete
    -Delete list item
    
    Close #12
    Close #24
    Close #25
    Close #27
    
    Co-authored-by: Shantala <mail2shantala@gmail.com>
    Co-authored-by: Joe <joe10994@gmail.com>
    Co-authored-by: Lindsay <linds234@gmail.com>

--- assets/scripts/list-items/events.js
+++ assets/scripts/list-items/events.js
@@ -1,6 +1,7 @@
 const getFormFields = require('../../../lib/get-form-fields')
 const api = require('./api')
 const ui = require('./ui')
+let tempId
 
 const onCreateListItem = (event) => {
   event.preventDefault()
@@ -8,6 +9,7 @@ const onCreateListItem = (event) => {
   const data = getFormFields(form)
   api.createListItem(data)
     .then(ui.createListItemSuccessful)
+    .then(onGetList)
     .catch(ui.createListItemFailed)
 }
 
@@ -16,8 +18,36 @@ const onGetList = () => {
     .then(ui.getListSuccessful) // fill in html
     .catch(ui.getListFailed)
 }
+
+const onUpdateListItem = (event) => {
+  event.preventDefault()
+  const form = event.target
+  const data = getFormFields(form)
+  api.updateListItem(tempId, data)
+    .then(onGetList)
+    .catch(ui.updateListItemFailed)
+    // .finally(console.log)
+}
+
+const onDeleteListItem = function (event) {
+  event.preventDefault()
+  const id = $(event.target).data('id')
+  api.deleteListItem(id)
+    .then(onGetList)
+    .catch(ui.deleteListItemFailed)
+}
+
+const getItemId = (event) => {
+  tempId = $(event.target).data('id')
+  console.log('HERE IS THE ID' + tempId)
+}
+
 const addEventHandlers = () => {
   $('#create-list').on('submit', onCreateListItem)
+  $('#update-item-form').on('submit', onUpdateListItem)
+  $('#bucket-list').on('click', '.delete-item-button', onDeleteListItem)
+  // get the ID of the list item and store it for use with onUpdateListItem
+  $('#bucket-list').on('click', '.update-item-button', getItemId)
 }
 
 module.exports = {
